5796d9a053b027efa1ab13c4e118ec33a423d418
Pj Meyer <pj.thehumanist@gmail.com>
2018-04-30 08:51:58 -0400

    Reload from clone, added to terrain, added waypoints

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadNextScene", 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadNextScene() {
+        
+        SceneManager.LoadScene(1);
+    }
+}
