c4576ee981dc84e00c4a51315ba39ca9a43c90d6
Pj Meyer <pj.thehumanist@gmail.com>
2018-05-29 00:21:38 -0400

    Added waypoints and ship movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,14 +5,23 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [Tooltip("In meters")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In meters")][SerializeField] float xRange = 4f;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, 
+            transform.localPosition.y, 
+            transform.localPosition.z);
 	}
 }
